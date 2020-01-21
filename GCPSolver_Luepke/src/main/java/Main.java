5ce1bfe4fc53b46886e5537a2137bc523504949b
flikkes <lpke.flx@gmail.com>
2020-01-21 19:39:20 +0100

    initial commit

---
+++ GCPSolver_Luepke/src/main/java/Main.java
@@ -0,0 +1,167 @@
+import parse.ProblemParser;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.StringWriter;
+import java.util.*;
+
+public class Main {
+
+    private static final String _1_FULL_INS_3 = "1-FullIns_3.col";
+    private static final String _1_FULL_INS_4 = "1-FullIns_4.col";
+    private static final String _2_FULL_INS_4 = "2-FullIns_4.col";
+    private static final String HOMER = "homer.col";
+    private static final String DSJC250_9 = "DSJC250.9.col";
+    private static final String FLAT300_28_0 = "flat300_28_0.col";
+    private static final String SIMPLE = "simple.col";
+    private static final String LE450_15A = "le450_15a.col";
+    private static final String DSJC250_1 = "DSJC250.1.col";
+    private static final String WAP_01A = "wap01a.col";
+
+    public static void main(String[] args) {
+        final String testFileName = WAP_01A;
+        final ProblemParser parser = new ProblemParser(testFileName);
+        int[] colors = new int[parser.getNodeCount()];
+
+        System.out.println("\n\n===================================SOLUTION===================================\n\n");
+
+        String durationString = "";
+        String chromNString = "";
+
+        for (int i = 1; i < 1001; i++) {
+            for (int j = 0; j < colors.length; j++) {
+                colors[j] = j;
+            }
+            System.out.println("Run " + i);
+            final long before = System.currentTimeMillis();
+            colors = plateauSearch(parser.getNodeConnections(), colors, getChromaticNumber(colors));
+            int chromN = getChromaticNumber(colors);
+            final long after = System.currentTimeMillis();
+            final long duration = after - before;
+            durationString += duration + "\n";
+            chromNString += chromN + "\n";
+            System.out.println("Duration (ms): " + duration);
+            System.out.println("Different colors: " + chromN);
+        }
+        System.out.println("Durations");
+        System.out.println(durationString);
+        System.out.println("Chromatic numbers");
+        System.out.println(chromNString);
+
+
+        final File solutionFile = new File(testFileName + ".sol");
+        try (final FileWriter writer = new FileWriter(solutionFile, false)) {
+            for (final int c : colors) {
+                writer.write(c + "\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static int[] solve(int node, int[][] nodeConnections, final int[] colors) {
+        final int[] newColors = Arrays.copyOf(colors, colors.length);
+        if (node < colors.length) {
+            for (int i = 0; i < colors.length; i++) {
+                if (isPossible(node, i, nodeConnections, colors)) {
+                    newColors[node] = i;
+                    return solve(node + 1, nodeConnections, newColors);
+                }
+            }
+            return solve(node, nodeConnections, newColors);
+        }
+        return newColors;
+    }
+
+    private static int[] solveNonTriangularFirstChild(int node, int[][] nodeConnections, final int[] colors) {
+        final int[] newColors = Arrays.copyOf(colors, colors.length);
+        if (node < colors.length) {
+            if (nodeConnections[node].length == 0) {
+                return solveNonTriangularFirstChild(node + 1, nodeConnections, newColors);
+            }
+            final int color = colors[node];
+            final int adjNode = nodeConnections[node][0];
+            for (int j = 0; j < nodeConnections[adjNode].length; j++) {
+                if (isPossible(nodeConnections[adjNode][j], color, nodeConnections, colors)) {
+                    newColors[nodeConnections[adjNode][j]] = color;
+                    return solveNonTriangularFirstChild(node + 1, nodeConnections, newColors);
+                }
+            }
+        }
+        return newColors;
+    }
+
+    private static int[] swapColors(int node, int[][] nodeConnections, final int[] colors) {
+        final int[] newColors = Arrays.copyOf(colors, colors.length);
+        if (node < colors.length) {
+            for (int i = 0; i < colors.length; i++) {
+                if (isPossible(node, colors[i], nodeConnections, colors) && i != node) {
+                    newColors[node] = colors[i];
+                    return swapColors(node + 1, nodeConnections, newColors);
+                }
+            }
+        }
+        return newColors;
+    }
+
+    private static int[] children(final int node, final int[][] nodeConnections, final int[] colors) {
+        if (node < colors.length) {
+            final int[] newColors = Arrays.copyOf(colors, colors.length);
+            if (isPossible(node, colors[node], nodeConnections, colors)) {
+                return children(node + 1, nodeConnections, colors);
+            }
+        }
+        return colors;
+    }
+
+    private static int[] plateauSearch(final int[][] nodeConnections, final int[] colors, final int chrN) {
+        final int[] newColors = Arrays.copyOf(colors, colors.length);
+        int chromaticNumber = chrN;
+        int newChromaticNumber = chrN;
+        int node = 0;
+        int limiter = 0;
+
+        while (newChromaticNumber <= chromaticNumber && limiter < newColors.length * 20) {
+            for (int dist = 1; dist < newColors.length + 1; dist++) {
+                final int swapNode = node + dist < newColors.length ? node + dist : node + dist - newColors.length;
+                if (newColors[node] != newColors[swapNode] && isPossible(node, newColors[swapNode], nodeConnections,
+                        newColors)) {
+                    newColors[node] = newColors[swapNode];
+                }
+            }
+            newChromaticNumber = getChromaticNumber(newColors);
+            chromaticNumber = newChromaticNumber;
+            node = node + 1 < newColors.length ? node + 1 : 0;
+            limiter = newChromaticNumber == chromaticNumber ? limiter + 1 : limiter;
+        }
+        return newColors;
+    }
+
+
+    private static int getChromaticNumber(int[] colors) {
+        int res = 1;
+        for (int i = 1; i < colors.length; i++) {
+            int j = 0;
+            for (j = 0; j < i; j++)
+                if (colors[i] == colors[j])
+                    break;
+            if (i == j)
+                res++;
+        }
+        return res;
+    }
+
+
+    private static boolean isPossible(int node, int color, int[][] nodeConnections, final int[] colors) {
+        if (color >= 0 && color < colors.length) {
+            for (final int n : nodeConnections[node]) {
+                if (colors[n] == color) {
+                    return false;
+                }
+            }
+            return true;
+        }
+        return false;
+    }
+}
---
+++ GCPSolver_Luepke/src/main/java/parse/ProblemParser.java
@@ -0,0 +1,52 @@
+package parse;
+
+import lombok.Getter;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
+@Getter
+public class ProblemParser {
+    private int nodeCount;
+    private int verticeCount;
+    private int[][] nodeConnections;
+
+    public ProblemParser(final String fileName) {
+        try (Stream<String> stream = Files
+                .lines(Paths.get(getClass().getClassLoader().getResource(fileName).getPath()))) {
+
+            stream.forEach(l -> {
+                if (!l.trim().isEmpty()) {
+                    if (l.startsWith("p")) {
+                        final String[] metaDataSplit = l.split(" ");
+                        this.nodeCount = Integer.parseInt(metaDataSplit[2]);
+                        this.verticeCount = Integer.parseInt(metaDataSplit[3]);
+                        this.nodeConnections = new int[this.nodeCount][0];
+                    } else if (l.startsWith("e")) {
+                        final String[] connectionSplit = l.split(" ");
+                        final int node = Integer.parseInt(connectionSplit[1]) - 1;
+                        final int connectedNode = Integer.parseInt(connectionSplit[2]) - 1;
+                        final int[] newConnectedNodes1 = new int[this.nodeConnections[node].length + 1];
+                        final int[] newConnectedNodes2 = new int[this.nodeConnections[connectedNode].length + 1];
+                        for (int i = 0; i < this.nodeConnections[node].length; i++) {
+                            newConnectedNodes1[i] = this.nodeConnections[node][i];
+                        }
+                        for (int i = 0; i < this.nodeConnections[connectedNode].length; i++) {
+                            newConnectedNodes2[i] = this.nodeConnections[connectedNode][i];
+                        }
+                        newConnectedNodes1[newConnectedNodes1.length - 1] = connectedNode;
+                        newConnectedNodes2[newConnectedNodes2.length - 1] = node;
+                        this.nodeConnections[node] = newConnectedNodes1;
+                        this.nodeConnections[connectedNode] = newConnectedNodes2;
+                    }
+                }
+            });
+
+        } catch (final IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
